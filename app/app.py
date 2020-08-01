d9da1d250e971aa41c233f24bc55e0216d5f2e33
mam32 <mam326@njit.edu>
2020-08-01 14:58:29 -0400

    Task:  Created app route for checkbox POST.. testing now

--- app/app.py
+++ app/app.py
@@ -13,5 +13,16 @@ app = Flask(__name__,template_folder='templates')
 def index():
     return render_template("index.html")
 
+@app.route("/",methods=['POST'])
+def other1():
+    symptoms = request.form.getlist('checkbox')
+    if len(symptoms) > 8:
+        flash('You do not meet the criteria for testing, have a nice day! Stay Healthy!')
+        return redirect("/")
+    else:
+        return redirect('/schedule')
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
