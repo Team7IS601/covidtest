1015cfea6f61ceed2117e131a751254a0188e8a6
mam32 <mam326@njit.edu>
2020-08-06 16:56:27 -0400

    Fix: Some code for register

--- app/app.py
+++ app/app.py
@@ -26,7 +26,7 @@ socketio = SocketIO(app)
 
 app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'fidelio'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'userData'
 mysql.init_app(app)
